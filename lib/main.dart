e666481d525714b7c41259935e175603d2cd4e6a
emmiabi <94717536+emmiabi@users.noreply.github.com>
2023-07-06 16:57:56 +0530

    Update main.dart
    commit by abi added commands

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,9 @@ class HomePage extends StatelessWidget {
           currentAccountPicture: CircleAvatar(
             backgroundImage:NetworkImage("https://images.unsplash.com/photo-1664575602554-2087b04935a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d29tYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"),
           ),),
+           //current account picture directly used with networkimage without using circular avator possible
+            //incase wrapup circular avatar that needs backgroundimage ,backgroundImage need ImageProvider.
+ 
 
           ListTile(
             leading: Icon(Icons.person),
