6545d7b92ed2361ca535437d1130faa20feb3aba
emmiabi <abiharini11@gmail.com>
2023-07-06 14:07:44 +0530

    abi

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,28 @@ class HomePage extends StatelessWidget {
         title:Text("awesome app"),
       ),
       body: Center(
-        child: Container(
-          child: Text("hi flutter"),
-        ),
+       child: Container(
+         padding: const EdgeInsets.all(12),
+         alignment: Alignment.center,
+         //color: Colors.teal,because  container have  boxdecoration dont put color//only one have color
+         width:100,
+         height: 100,
+         decoration:BoxDecoration(
+             borderRadius: BorderRadius.circular(10),//box corner shaped
+             boxShadow: [BoxShadow(color: Colors.blueGrey,blurRadius: 8,offset:Offset(2.0,5.0)/*spreadRadius: 10*/)],
+
+           //shape: BoxShape.circle,
+           color: Colors.teal,
+           gradient: LinearGradient(colors: [Colors.yellow,Colors.deepOrangeAccent])
+         ),
+         child: Text("I am a box",
+           textAlign:TextAlign.center,
+           style: TextStyle(color: Colors.black54,fontWeight:FontWeight.bold,fontSize:20 ),),
+       ),
       ),
     );
   }
 }
 
 
+//container and text
